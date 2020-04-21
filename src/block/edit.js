8f2773a506f4ed236f0041c2d2ff185f49e34f16
Michaël Perrin <contact@michaelperrin.fr>
2020-04-20 19:48:28 -0500

    Use React hooks

--- src/block/edit.js
+++ src/block/edit.js
@@ -1,13 +1,8 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import {
-	Component,
+	useRef,
 } from '@wordpress/element';
 import {
 	withNotices,
@@ -46,149 +41,144 @@ const INNER_BLOCKS_TEMPLATE = [
 	} ],
 ];
 
-class CoverEdit extends Component {
-	constructor() {
-		super(...arguments);
-		this.onUploadError = this.onUploadError.bind(this);
-	}
+const CoverEdit = (props) => {
+	const videoRef = useRef(null);
 
-	onUploadError(message) {
-		const { noticeOperations } = this.props;
+	const onUploadError = (message) => {
+		const { noticeOperations } = props;
 		noticeOperations.removeAllNotices();
 		noticeOperations.createErrorNotice(message);
 	}
 
-	render() {
-		const {
-			attributes,
-			setAttributes,
-			className,
-			noticeUI,
-			overlayColor,
-			setOverlayColor,
-		} = this.props;
+	const {
+		attributes,
+		setAttributes,
+		className,
+		noticeUI,
+		overlayColor,
+		setOverlayColor,
+	} = props;
 
-		const {
-			backgroundType,
-			minHeight,
-			url,
-		} = attributes;
+	const {
+		backgroundType,
+		minHeight,
+		url,
+	} = attributes;
 
-		const onSelectMedia = (media) => {
-			if (!media || !media.url) {
-				setAttributes({ url: undefined, id: undefined });
+	const onSelectMedia = (media) => {
+		if (!media || !media.url) {
+			setAttributes({ url: undefined, id: undefined });
+			return;
+		}
+		let mediaType;
+		// for media selections originated from a file upload.
+		if (media.media_type) {
+			if (media.media_type === IMAGE_BACKGROUND_TYPE) {
+				mediaType = IMAGE_BACKGROUND_TYPE;
+			} else {
+				// only images and videos are accepted so if the media_type is not an image we can assume it is a video.
+				// Videos contain the media type of 'file' in the object returned from the rest api.
+				mediaType = VIDEO_BACKGROUND_TYPE;
+			}
+		} else { // for media selections originated from existing files in the media library.
+			if (
+				media.type !== IMAGE_BACKGROUND_TYPE &&
+				media.type !== VIDEO_BACKGROUND_TYPE
+			) {
 				return;
 			}
-			let mediaType;
-			// for media selections originated from a file upload.
-			if (media.media_type) {
-				if (media.media_type === IMAGE_BACKGROUND_TYPE) {
-					mediaType = IMAGE_BACKGROUND_TYPE;
-				} else {
-					// only images and videos are accepted so if the media_type is not an image we can assume it is a video.
-					// Videos contain the media type of 'file' in the object returned from the rest api.
-					mediaType = VIDEO_BACKGROUND_TYPE;
-				}
-			} else { // for media selections originated from existing files in the media library.
-				if (
-					media.type !== IMAGE_BACKGROUND_TYPE &&
-					media.type !== VIDEO_BACKGROUND_TYPE
-				) {
-					return;
-				}
-				mediaType = media.type;
-			}
-
-			setAttributes({
-				url: media.url,
-				id: media.id,
-				backgroundType: mediaType,
-				...(mediaType === VIDEO_BACKGROUND_TYPE ?
-					{ focalPoint: undefined, hasParallax: undefined } :
-					{}
-				),
-			});
-		};
-
-		const style = {
-			...(
-				backgroundType === IMAGE_BACKGROUND_TYPE ?
-					backgroundImageStyles(url) :
-					{}
-			),
-			backgroundColor: overlayColor.color,
-			minHeight,
-		};
-
-		const hasBackground = !!(url || overlayColor.color);
-
-		if (!hasBackground) {
-			const placeholderIcon = <BlockIcon icon={icon} />;
-			const label = __('Slider');
-
-			return (
-				<>
-					{controls}
-					<MediaPlaceholder
-						icon={placeholderIcon}
-						className={className}
-						labels={{
-							title: label,
-							instructions: __('Upload an image or video file, or pick one from your media library.'),
-						}}
-						onSelect={onSelectMedia}
-						accept="image/*,video/*"
-						allowedTypes={ALLOWED_MEDIA_TYPES}
-						notices={noticeUI}
-						onError={this.onUploadError}
-					>
-						<div
-							className="wp-block-cover__placeholder-background-options"
-						>
-							<ColorPalette
-								disableCustomColors={true}
-								value={overlayColor.color}
-								onChange={setOverlayColor}
-								clearable={false}
-							/>
-						</div>
-					</MediaPlaceholder>
-				</>
-			);
+			mediaType = media.type;
 		}
 
-		const classes = className;
+		setAttributes({
+			url: media.url,
+			id: media.id,
+			backgroundType: mediaType,
+			...(mediaType === VIDEO_BACKGROUND_TYPE ?
+				{ focalPoint: undefined, hasParallax: undefined } :
+				{}
+			),
+		});
+	};
+
+	const style = {
+		...(
+			backgroundType === IMAGE_BACKGROUND_TYPE ?
+				backgroundImageStyles(url) :
+				{}
+		),
+		backgroundColor: overlayColor.color,
+		minHeight,
+	};
+
+	const hasBackground = !!(url || overlayColor.color);
+
+	if (!hasBackground) {
+		const placeholderIcon = <BlockIcon icon={icon} />;
+		const label = __('Slider!!');
 
 		return (
 			<>
 				{controls}
-				<div>
-
+				<MediaPlaceholder
+					icon={placeholderIcon}
+					className={className}
+					labels={{
+						title: label,
+						instructions: __('Upload an image or video file, or pick one from your media library.'),
+					}}
+					onSelect={onSelectMedia}
+					accept="image/*,video/*"
+					allowedTypes={ALLOWED_MEDIA_TYPES}
+					notices={noticeUI}
+					onError={onUploadError}
+				>
 					<div
-						data-url={url}
-						style={style}
-						className={classes}
+						className="wp-block-cover__placeholder-background-options"
 					>
-						{VIDEO_BACKGROUND_TYPE === backgroundType && (
-							<video
-								ref={this.videoRef}
-								className="wp-block-cover__video-background"
-								autoPlay
-								muted
-								loop
-								src={url}
-							/>
-						)}
-						<div className="wp-block-cover__inner-container">
-							<InnerBlocks
-								template={INNER_BLOCKS_TEMPLATE}
-							/>
-						</div>
+						<ColorPalette
+							disableCustomColors={true}
+							value={overlayColor.color}
+							onChange={setOverlayColor}
+							clearable={false}
+						/>
 					</div>
-				</div>
+				</MediaPlaceholder>
 			</>
 		);
 	}
+
+	const classes = className;
+
+	return (
+		<>
+			{controls}
+			<div>
+
+				<div
+					data-url={url}
+					style={style}
+					className={classes}
+				>
+					{VIDEO_BACKGROUND_TYPE === backgroundType && (
+						<video
+							ref={videoRef}
+							className="wp-block-cover__video-background"
+							autoPlay
+							muted
+							loop
+							src={url}
+						/>
+					)}
+					<div className="wp-block-cover__inner-container">
+						<InnerBlocks
+							template={INNER_BLOCKS_TEMPLATE}
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default compose([
